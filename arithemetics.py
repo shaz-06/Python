0a911993fe532ea0238463318b8e5ced4302ddd8
shaz-06 <shashankshetty3219@gmail.com>
2025-03-12 19:41:38 +0530

    Add files via upload
    Program to do Arithemetic Operations

---
+++ arithemetics.py
@@ -0,0 +1,15 @@
+# program to do arithemetics operations
+
+a=int(input("Enter a: "))
+b=int(input("enter b: "))
+sum=a+b
+print(sum)
+sub=a-b
+print(sub)
+div=a/b
+print(div)
+qou=a//b
+print(qou)
+rem=a%b
+print(rem)
+
